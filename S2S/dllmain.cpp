8039bcc0f6644df983c64f0805d71178b8a2a1ba
FrexCheat <admin@frexlink.com>
2024-07-28 23:31:48 +0800

    添加项目文件。

---
+++ S2S/dllmain.cpp
@@ -0,0 +1,166 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <iostream>
+BYTE buff[0x1000];
+ULONG64(__fastcall* oCreateMessage)(ULONG64,ULONG64);
+void(__fastcall* oIncRef)(void*);
+void(__fastcall* oDecRef)(void*);
+ULONG64 (__fastcall* oRead)(void*);
+DWORD32(__fastcall* oSize)(void*);
+void(__fastcall* oWriteBits)(void*, DWORD, int);
+void(__fastcall* oWriteBits64)(void*, ULONG64, int);
+void(__fastcall* oWriteData)(void*, const void*, int);
+void(__fastcall* oWriteString)(void*, const char*);
+void(__fastcall* oWriteCompLTVector)(void*, const LTVector&);
+void(__fastcall* oWriteCompPos)(void*, const LTVector&);
+void(__fastcall* oWriteObject)(void*, PVOID);
+
+
+
+ULONG64 __fastcall HookRead(void* LTMessage_Write) {
+    printf("Msg->Read();\n");
+    return oRead(LTMessage_Write);
+}
+DWORD32 __fastcall HookSize(void* LTMessage_Write) {
+
+    printf("Msg->Size();\n");
+    return oSize(LTMessage_Write);
+}
+void __fastcall HookIncRef(void* LTMessage_Write) {
+    printf("Msg->IncRef();\n");
+    return oIncRef(LTMessage_Write);
+}
+void __fastcall HookDecRef(void* LTMessage_Write) {
+    printf("Msg->DecRef();\n\n");
+    return oDecRef(LTMessage_Write);
+}
+void __fastcall HookWriteBits(void* LTMessage_Write, DWORD nValue, int nSize) {
+    if (nValue < 0x1000)
+        printf("Msg->WriteBits(0x%lX,%d);\n", nValue, nSize);
+    return oWriteBits(LTMessage_Write, nValue, nSize);
+}
+void __fastcall HookWriteBits64(void* LTMessage_Write, ULONG64 nValue, int nSize) {
+    printf("Msg->WriteBits64(0x%llX,%d);\n", nValue, nSize);
+    return oWriteBits64(LTMessage_Write, nValue, nSize);
+}
+void __fastcall HookWriteData(void* LTMessage_Write, const void* pData, int nSize) {
+    printf("X->%.1f | Y->%.1f | Z->%.1f\n", *(float*)(ULONG64(pData)), *(float*)(ULONG64(pData) + 4), *(float*)(ULONG64(pData) + 8));
+    printf("Msg->WriteData(0x%llX,%d);\n", pData, nSize);
+    return oWriteData(LTMessage_Write, pData, nSize);
+}
+void __fastcall HookWriteString(void* LTMessage_Write, const char* pString) {
+    printf("Msg->WriteString(%s);\n", pString);
+    return oWriteString(LTMessage_Write, pString);
+}
+void __fastcall HookWriteCompLTVector(void* LTMessage_Write, const LTVector& vVec) {
+    printf("X->%.1f | Y->%.1f | Z->%.1f\n", vVec.x, vVec.y, vVec.z);
+    printf("Msg->WriteCompLTVector(xyz);\n");
+    return oWriteCompLTVector(LTMessage_Write, vVec);
+}
+void __fastcall HookWriteCompPos(void* LTMessage_Write, const LTVector& vPos) {
+    printf("X->%.1f | Y->%.1f | Z->%.1f\n", vPos.x, vPos.y, vPos.z);
+    printf("Msg->WriteCompPos(xyz);\n");
+    return oWriteCompPos(LTMessage_Write, vPos);
+}
+void __fastcall HookWriteObject(void* LTMessage_Write, PVOID hObj) {
+    printf("Msg->WriteObject(0x%llX,%d);\n", hObj);
+    return oWriteObject(LTMessage_Write, hObj);
+}
+
+
+
+
+
+ULONG64 CSHELL;
+
+ULONG64 __fastcall HookCreateMessage(ULONG64 RCX, ULONG64 Message) {
+    auto result = oCreateMessage(RCX, Message);
+    auto v1 = *(ULONG64*)Message;
+    auto v2 = *(ULONG64*)v1;
+    if (v1 != (ULONG64)buff) {
+        memcpy(buff, (PVOID)v2, 0x1000);
+        oIncRef = reinterpret_cast<decltype(oIncRef)>(*(ULONG64*)(v2 + 0x08));
+        oDecRef = reinterpret_cast<decltype(oIncRef)>(*(ULONG64*)(v2 + 0x10));
+        oRead = reinterpret_cast<decltype(oRead)>(*(ULONG64*)(v2 + 0x38));
+        oSize = reinterpret_cast<decltype(oSize)>(*(ULONG64*)(v2 + 0x40));
+        oWriteBits = reinterpret_cast<decltype(oWriteBits)>(*(ULONG64*)(v2 + 0x48));
+        oWriteBits64 = reinterpret_cast<decltype(oWriteBits64)>(*(ULONG64*)(v2 + 0x50));
+        oWriteData = reinterpret_cast<decltype(oWriteData)>(*(ULONG64*)(v2 + 0x58));
+        oWriteString = reinterpret_cast<decltype(oWriteString)>(*(ULONG64*)(v2 + 0x70));
+        oWriteCompLTVector = reinterpret_cast<decltype(oWriteCompLTVector)>(*(ULONG64*)(v2 + 0x80));
+        oWriteCompPos = reinterpret_cast<decltype(oWriteCompPos)>(*(ULONG64*)(v2 + 0x88));
+        oWriteObject = reinterpret_cast<decltype(oWriteObject)>(*(ULONG64*)(v2 + 0xB8));
+        *(PVOID*)(buff + 0x08) = HookIncRef;
+        *(PVOID*)(buff + 0x10) = HookDecRef;
+        *(PVOID*)(buff + 0x38) = HookRead;
+        *(PVOID*)(buff + 0x40) = HookSize;
+        *(PVOID*)(buff + 0x48) = HookWriteBits;
+        *(PVOID*)(buff + 0x50) = HookWriteBits64;
+        *(PVOID*)(buff + 0x58) = HookWriteData;
+        *(PVOID*)(buff + 0x70) = HookWriteString;
+        *(PVOID*)(buff + 0x80) = HookWriteCompLTVector;
+        *(PVOID*)(buff + 0x88) = HookWriteCompPos;
+        *(PVOID*)(buff + 0xB8) = HookWriteObject;
+        *(ULONG64*)v1 = (ULONG64)buff;
+    }
+    return result;
+}
+typedef bool(__fastcall* WeaponFunc)(ULONG64* a1, BYTE* a2, int a3);
+WeaponFunc Func = nullptr;
+ULONG64 PlayCInt, WeaponMgr;
+void FuncStart() {
+    auto addr = *(ULONG64**)PlayCInt;
+    if (!addr)return;
+    //printf("PlayeCInt:%p\n", addr);
+    auto weaponaddr = *(ULONG64*)WeaponMgr;
+    if (!weaponaddr) return;
+    //printf("weaponaddr:%p\n", weaponaddr);
+    auto weapon = *(ULONG64**)(weaponaddr + 0x6D20);
+    //printf("weapon:%p\n", weapon);
+    Func(addr, (BYTE*)weapon, 1);
+}
+
+
+BYTE buff1[0x1000];
+void MainThrad() {
+    while (true) {
+        if (GetAsyncKeyState(VK_HOME) & 1)
+            break;
+    }
+    CSHELL = ULONG64(GetModuleHandleA("cshell_x64.dll"));
+    auto CLTCommonClient = *(ULONG64*)(0x14042C778);
+    auto v2 = *(ULONG64*)(CLTCommonClient);
+    oCreateMessage = reinterpret_cast<decltype(oCreateMessage)>(*(ULONG64*)(v2 + 0x48));
+
+    memcpy(buff1, PVOID(v2), 0x1000);
+
+    *(PVOID*)(buff1 + 0x48) = HookCreateMessage;
+    *(ULONG64*)CLTCommonClient = (ULONG64)buff1;
+
+    //PlayCInt = CSHELL + 0x2733B28;
+    //WeaponMgr = CSHELL + 0x26DD790;
+    //Func = (WeaponFunc)(CSHELL + 0x7898E0);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        AllocConsole();
+        freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+        freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+        SetConsoleTitleA("Frex S2S Debug");
+        InstallSafeThread(MainThrad);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ S2S/pch.h
@@ -0,0 +1,152 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include <winternl.h>
+#include <TlHelp32.h>
+#include <TlHelp32.h>	//CreateToolhelp32Snapshot
+#pragma comment(lib,"ntdll.lib" )
+#endif //PCH_H
+class LTVector
+{
+public:
+	float                                              x;
+	float                                              y;
+	float                                              z;
+
+	inline LTVector() : x(0), y(0), z(0) {}
+
+	inline LTVector(float x, float y, float z) : x(x), y(y), z(z) {}
+
+	inline LTVector operator + (const LTVector& other) const { return LTVector(x + other.x, y + other.y, z + other.z); }
+
+	inline LTVector operator - (const LTVector& other) const { return LTVector(x - other.x, y - other.y, z - other.z); }
+
+	inline LTVector operator * (float scalar) const { return LTVector(x * scalar, y * scalar, z * scalar); }
+
+	inline LTVector operator * (const LTVector& other) const { return LTVector(x * other.x, y * other.y, z * other.z); }
+
+	inline LTVector operator / (float scalar) const { return LTVector(x / scalar, y / scalar, z / scalar); }
+
+	inline LTVector operator / (const LTVector& other) const { return LTVector(x / other.x, y / other.y, z / other.z); }
+
+	inline LTVector& operator=  (const LTVector& other) { x = other.x; y = other.y; z = other.z; return *this; }
+
+	inline LTVector& operator+= (const LTVector& other) { x += other.x; y += other.y; z += other.z; return *this; }
+
+	inline LTVector& operator-= (const LTVector& other) { x -= other.x; y -= other.y; z -= other.z; return *this; }
+
+	inline LTVector& operator*= (const float other) { x *= other; y *= other; z *= other; return *this; }
+
+
+
+	friend bool operator==(const LTVector& first, const LTVector& second) { return first.x == second.x && first.y == second.y && first.z == second.z; }
+
+	friend bool operator!=(const LTVector& first, const LTVector& second) { return !(first == second); }
+};
+inline BOOL InstallSafeThread(
+	__in LPVOID function
+) {
+
+	HMODULE hModule = GetModuleHandleA(0);
+
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, GetCurrentProcessId());
+	if (hSnapshot == INVALID_HANDLE_VALUE)
+		return FALSE;
+	THREADENTRY32 te32 = { 0,0,0,0,0,0,0 };
+	te32.dwSize = sizeof(THREADENTRY32);
+	if (!Thread32First(hSnapshot, &te32))
+	{
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+	do
+	{
+		HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, false, te32.th32ThreadID);
+		if (hThread)
+		{
+			DWORD_PTR dwStartAddress = 0;
+			if (NT_SUCCESS(NtQueryInformationThread(hThread, static_cast<THREADINFOCLASS>(9), &dwStartAddress, sizeof(DWORD_PTR), NULL)))
+			{
+				PIMAGE_DOS_HEADER DosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(hModule);
+				PIMAGE_NT_HEADERS NtHeader = reinterpret_cast<PIMAGE_NT_HEADERS>((reinterpret_cast<DWORD_PTR>(hModule) + DosHeader->e_lfanew));
+				if (!NtHeader || NtHeader->Signature != IMAGE_NT_SIGNATURE)
+				{
+					SetLastError(1793);
+					return FALSE;
+				}
+				if (dwStartAddress > reinterpret_cast<DWORD_PTR>(hModule) &&
+					dwStartAddress < (reinterpret_cast<DWORD_PTR>(hModule + 0x1000) + NtHeader->OptionalHeader.SizeOfImage))
+				{
+					//MessageBoxA(0, 0, 0, 0);
+					_MEMORY_BASIC_INFORMATION mbi;
+					CONTEXT ctx;
+					DWORD Old = 0;
+					ZeroMemory(&mbi, sizeof(mbi));
+					if (!VirtualQuery(reinterpret_cast<LPCVOID>(dwStartAddress), &mbi, sizeof(mbi)))
+					{
+						CloseHandle(hSnapshot);
+						CloseHandle(hThread);
+						return FALSE;
+					}
+					if (!VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &Old))
+					{
+						CloseHandle(hSnapshot);
+						CloseHandle(hThread);
+						return FALSE;
+
+					}
+					HANDLE Thread = CreateRemoteThread(GetCurrentProcess(), 0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(dwStartAddress), 0, CREATE_SUSPENDED, 0);
+					if (Thread == 0 || Thread == INVALID_HANDLE_VALUE)
+					{
+						TerminateThread(Thread, 0);
+						CloseHandle(Thread);
+						CloseHandle(hSnapshot);
+						CloseHandle(hThread);
+						return FALSE;
+					}
+					ctx.ContextFlags = CONTEXT_ALL;
+					if (!GetThreadContext(Thread, &ctx))
+					{
+						TerminateThread(Thread, 0);
+						CloseHandle(Thread);
+						CloseHandle(hSnapshot);
+						CloseHandle(hThread);
+						return FALSE;
+					}
+#ifdef _WIN64
+					ctx.Rcx = reinterpret_cast<DWORD64>(function);
+#else
+					ctx.Eax = reinterpret_cast<DWORD>(function);
+#endif
+
+					if (!SetThreadContext(Thread, &ctx))
+					{
+						TerminateThread(Thread, 0);
+						CloseHandle(Thread);
+						CloseHandle(hSnapshot);
+						CloseHandle(hThread);
+						return FALSE;
+					}
+					ResumeThread(Thread);
+					CloseHandle(Thread);
+					CloseHandle(hSnapshot);
+					CloseHandle(hThread);
+					return TRUE;
+				}
+
+			}
+			CloseHandle(hThread);
+		}
+	} while (Thread32Next(hSnapshot, &te32));
+	CloseHandle(hSnapshot);
+	return FALSE;
+
+}
